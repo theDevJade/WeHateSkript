9f2808c77bfc35e838d23aac60ab6d3448982e57
theDevJade <jadeisstolen@gmail.com>
2023-10-31 17:07:52 -0700

    Initial commit

---
+++ src/main/java/com/thedevjade/wehateskript/WeHateSkript.kt
@@ -0,0 +1,13 @@
+package com.thedevjade.wehateskript
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class WeHateSkript : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
