bc0e5e862b6ab15b678b6e1b208ea3e98711e762
theDevJade <jadeisstolen@gmail.com>
2023-10-31 17:08:38 -0700

    wow

--- src/main/java/com/thedevjade/wehateskript/WeHateSkript.kt
+++ src/main/java/com/thedevjade/wehateskript/WeHateSkript.kt
@@ -1,13 +1,47 @@
 package com.thedevjade.wehateskript
 
+import org.bukkit.plugin.Plugin
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
 
 class WeHateSkript : JavaPlugin() {
+
     override fun onEnable() {
         // Plugin startup logic
+        info("WeHateSkript is enabled!")
+        info("Looking around...")
+
+        val skriptPlugin = server.pluginManager.getPlugin("Skript")
+
+        if (skriptPlugin != null) {
+            disableAndDeleteSkript(skriptPlugin)
+        } else {
+            info("Skript is not installed!")
+        }
     }
 
     override fun onDisable() {
         // Plugin shutdown logic
+    }
+
+    private fun disableAndDeleteSkript(skriptPlugin: Plugin) {
+        info("Skript is installed!")
+        info("Disabling Skript...")
+
+        val jarFile = File(skriptPlugin.javaClass.protectionDomain.codeSource.location.toURI().path)
+
+        server.pluginManager.disablePlugin(skriptPlugin)
+        info("Skript is disabled!")
+
+        info("Deleting Skript...")
+        jarFile.delete()
+        info("Skript is deleted!")
+
+        info("We Hate Skript :D")
+        server.shutdown()
+    }
+
+    private fun info(message: String) {
+        logger.info(message)
     }
 }
